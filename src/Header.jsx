c777c1df43cb37f11c37202faf9bf5c24713b26a
AsiKrinitz <AsiKrinitz@gmail.com>
2025-06-10 16:10:16 +0300

    1st upload of my React Project

---
+++ src/Header.jsx
@@ -0,0 +1,22 @@
+function Header() {
+  return (
+    <header>
+      <h1>This is my 1st React Project</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.google.com">Home</a>
+          </li>
+          <li>
+            <a href="https://www.google.com">About</a>
+          </li>
+          <li>
+            <a href="https://www.google.com">Contact Us</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
