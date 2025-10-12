5deb74827c415f0893761bcd57f4c2e534e21ba3
AsiKrinitz <AsiKrinitz@gmail.com>
2025-10-12 12:29:14 +0300

    add footer comp to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./Header";
+import Footer from "./footer";
 
 function App() {
-  return <Header></Header>;
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+import "./Footer.css";
+
+function Footer() {
+  return <h2 className="footer"> This is my website;</h2>;
+}
+
+export default Footer;
