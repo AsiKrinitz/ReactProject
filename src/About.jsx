9059c9b58aa4b8e9cd3d4cda1683909fd6cc1f7d
AsiKrinitz <AsiKrinitz@gmail.com>
2025-10-29 22:14:11 +0200

    deploy again becuase the resume...

--- src/About.jsx
+++ src/About.jsx
@@ -1,14 +1,7 @@
 import "./About.css";
 
 function About() {
-  return (
-    <div className="aboutContainer">
-      <h1 className="aboutTitle">מי אני?</h1>
-      <div className="aboutCard">
-        <p>אז אני אסי קריניץ,</p>
-      </div>
-    </div>
-  );
+  return <div className="aboutContainer"></div>;
 }
 
 export default About;
