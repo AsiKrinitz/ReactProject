a5bb23bb11d70dc22f7cf1ad4d5f273912f0f793
AsiKrinitz <AsiKrinitz@gmail.com>
2025-11-22 22:01:29 +0200

    adding some pictures, and make it random picture every time. and also imporve  the home page.

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,13 +1,34 @@
 import "./Home.css";
+import { useState, useEffect } from "react";
+
+import Asi1 from "../assets/Asi1.jpg";
+import Asi2 from "../assets/Asi2.jpg";
+import Asi3 from "../assets/Asi3.jpg";
+import Asi4 from "../assets/Asi4.jpg";
 import Asi5 from "../assets/Asi5.jpeg";
+import Asi6 from "../assets/Asi6.jpeg";
+import Asi7 from "../assets/Asi7.jpeg";
+import Asi8 from "../assets/Asi8.jpeg";
+import Asi9 from "../assets/Asi9.jpeg";
+import Asi10 from "../assets/Asi10.jpeg";
+
+const photos = [Asi1, Asi2, Asi3, Asi4, Asi5, Asi6, Asi7, Asi8, Asi9, Asi10];
 
 function Home() {
+  const [randomPhoto, setRandomPhoto] = useState("");
+
+  useEffect(() => {
+    const picked = photos[Math.floor(Math.random() * photos.length)];
+    setRandomPhoto(picked);
+  }, []);
+
   return (
     <div className="home">
       <h1 className="home-title">אהלן חברים! ברוכים הבאים לאתר שלי 👋</h1>
 
       <div className="intro-card">
         <div className="text-content">
+          <h2 className="sub-title">קצת עליי</h2>
           <p>
             נעים להכיר, אני <strong>אסי קריניץ</strong>, בן 29 מקריית חיים, חי
             עם בת זוגתי המדהימה והכלב שלנו ♥
@@ -38,17 +59,32 @@ function Home() {
 
           <p>
             לאחרונה הרחבתי את הידע שלי גם לתחום החומרה והמערכות, במסגרת קורס{" "}
-            <strong>טכנאי מחשבים</strong> במכללה למנהל בחיפה.
+            <strong>טכנאי מחשבים</strong> במכללה למנהל בחיפה, אז יצא שאני גם
+            מתכנת וגם טכנאי מחשבים =)
           </p>
 
           <p>
-            אני תמיד מחפש את האתגר הבא – מקום שאוכל להמשיך לצמוח בו, ללמוד
-            ולהשפיע. אם אהבתם את מה שקראתם ומרגישים שאוכל להשתלב אצלכם, אשמח
-            לשוחח ♥
+            חוץ מעולם התכנות והמחשבים, אני אוהב לשחק פינג פונג ואני גם מתאמן
+            באופן קבוע, לפני זה התאמנתי תקופה באיגרוף תאילנדי וגם MMA ובכללי אני
+            אוהב לצפות ב UFC.
+            <br></br>
+            בנוסף בשנה האחרונה ניכנסתי לעולם שוק ההון, השקעות ומסחר, לקחתי את זה
+            מאוד ברצינות וכמו כל דבר שאני עושה בחיים אני מנסה לעשות הכי טוב שאני
+            יכול! וגם מתי שלא הולך אני לא מוותר, לומד מטעויות וממשיך הלאה!
+            <br></br>
+            וכמובן שאני חולה על לטייל בארץ המדהימה שלנו ובעולם כולו!
+          </p>
+
+          <p>
+            עכשיו אני מחפש את האתגר הבא שלי – מקום שאוכל להמשיך לצמוח בו, ללמוד
+            ולהשפיע.
+            <br></br>
+            אם אהבתם את מה שקראתם ומרגישים שאוכל להשתלב אצלכם, אשמח לשוחח ♥
           </p>
         </div>
-
-        <img src={Asi5} alt="אסי קריניץ" className="profile-image" />
+        {randomPhoto && (
+          <img src={randomPhoto} alt="אסי קריניץ" className="profile-image" />
+        )}{" "}
       </div>
     </div>
   );
